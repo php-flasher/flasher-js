f063b844612643220a9a9234f7807754e230614e
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2023-06-14 08:30:08 +0100

    Wip

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,23 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        project: './tsconfig.json',
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  plugins: [
+    '@typescript-eslint',
+    'import',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {},
+  settings: {
+    'import/resolver': {
+      typescript: {},
     },
-    ignorePatterns: ['.eslintrc.js'],
-    plugins: [
-        '@typescript-eslint',
-        'import',
-    ],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:import/errors',
-        'plugin:import/warnings',
-        'plugin:import/typescript',
-    ],
-    rules: {},
-    settings: {
-        'import/resolver': {
-            typescript: {}
-        },
-    },
-};
+  },
+}
